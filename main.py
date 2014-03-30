56119224b749a05dd0e25a4461fed0f2868fda6e
medowd <medowd@ockmail.com>
2014-03-29 21:19:50 -0400

    main revised

--- main.py
+++ main.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
+"""import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -22,4 +22,55 @@ class MainHandler(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
+], debug=True)"""
+
+
+import cgi
+
+from google.appengine.api import users
+
+import webapp2
+
+
+MAIN_PAGE_HTML = """\
+<html>
+  <body>
+    <form action="/sign" method="post">
+      <html><body>name
+      <div><textarea name="fName" rows="1" cols="15"></textarea> <textarea name="lName" rows="1" cols="15"></textarea> </div>
+      <html><body>phone number 
+      <div><textarea name="number" rows="1" cols="30"></textarea></div>
+      <html><body>email
+      <div><textarea name="email" rows="1" cols="30"></textarea></div>
+      <div><input type="submit" value="Submit"></div>
+    </form>
+  </body>
+</html>
+"""
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.write(MAIN_PAGE_HTML)
+
+
+class SignUp(webapp2.RequestHandler):
+
+    def post(self):
+        self.response.write('<html><body>You wrote:<pre>')
+        self.response.write(cgi.escape(self.request.get('fName')))
+        self.response.write(' ')
+        self.response.write(cgi.escape(self.request.get('lName')))
+        self.response.write('\n')
+        self.response.write(cgi.escape(self.request.get('number')))
+        self.response.write('\n')
+        self.response.write(cgi.escape(self.request.get('email')))
+        self.response.write('</pre></body></html>')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/sign', SignUp),
 ], debug=True)
+
