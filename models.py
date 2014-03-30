babd861594fb9f1f5a31c343bad67f592d4b9f32
robertvann <rvanncollege@gmail.com>
2014-03-29 21:53:11 -0400

    Models

--- models.py
+++ models.py
@@ -1,9 +1,25 @@
+import cgi
+import urllib
 import webapp2
-from google.appengine.ext import db
 
-class MainPage(webapp2.RequestHandler):
-  def get(self):
-    self.response.out.write('<html><body>')
-    greetings = db.GqlQuery("SELECT * "
-                            "FROM Greeting "
-                            "ORDER BY date DESC LIMIT 10")
+from google.appengine.ext import ndb
+from google.appengine.api import users
+
+class Responder(ndb.Model):
+  """USER INFORMATION ENTITY"""
+  firstName = ndb.StringProperty()
+  lastName = ndb.StringProperty()
+  phoneNum = ndb.StringProperty()
+  email = ndb.StringProperty
+
+  @classmethod
+  def query_book(cls, ancestor_key):
+    return cls.query(ancestor=ancestor_key).order(-cls.date)
+
+class Event(ndb.Model):
+	"""BLUE BUTTON PUSH INFORMATION"""
+	location = GeoPtProperty
+	datetime = DateTimeProperty
+	
+
+
